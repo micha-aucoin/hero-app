6573dde3d5cbe946a704e6c02ad56af5ffc2a2fe
micha-aucoin <maucoin94@gmail.com>
2023-03-08 11:55:54 -0600

    update Dockerfile

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,17 @@ from dotenv import load_dotenv
 
 from app.core.config import Settings
 
-load_dotenv(getenv("ENV_FILE"))
+if getenv('ENV_FILE'):
+    load_dotenv(getenv("ENV_FILE"))
+
+else:
+    getenv("API_V1_PREFIX")
+    getenv("DEBUG")
+    getenv("PROJECT_NAME")
+    getenv("VERSION")
+    getenv("DESCRIPTION")
+    getenv("DB_ASYNC_CONNECTION_STR")
+    getenv("DB_ASYNC_TEST_CONNECTION_STR")
+    getenv("DB_EXCLUDE_TABLES")
 
 settings = Settings()
