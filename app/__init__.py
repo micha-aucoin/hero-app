119d2e7a346114165baf9437710cac85b455d465
micha-aucoin <maucoin94@gmail.com>
2023-05-06 10:33:13 -0500

    refactor

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from os import getenv
 
+from app.core.config import Settings
 from dotenv import load_dotenv
 
-from app.core.config import Settings
-
-load_dotenv(getenv("ENV_FILE"))
+load_dotenv()
 
 settings = Settings()
